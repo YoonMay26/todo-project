3746bdd8380270d58174ce4f0f61bf8b6f5e9ed3
YOON <ysj20152@gmail.com>
2024-11-12 11:21:05 +0900

    created -> deadline

--- todolist/todo/models.py
+++ todolist/todo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True)
     complete = models.BooleanField(default=False)
     important = models.BooleanField(default=False)
 
