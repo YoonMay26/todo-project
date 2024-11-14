ed2916ab8d8078877d449ea7dc57546c8355a526
YoonMay26 <ysj20152@gmail.com>
2024-11-14 10:23:50 +0900

    coding_final

--- todolist/todo/templatetags/custom_filters.py
+++ todolist/todo/templatetags/custom_filters.py
@@ -1,12 +1,20 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter
-def get_item(dictionary, key):
-    return dictionary.get(key, '')
+
 
 @register.filter(name='add_class')
 def add_class(value, arg):
     return value.as_widget(attrs={'class': arg})
 
+
+
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+@register.filter(name='add_checked_attribute')
+def add_checked_attribute(widget):
+    return mark_safe(widget.replace('>', ' checked>'))
